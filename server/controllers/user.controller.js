2cb492cfb703dc15a28771dddf93c9dd1c348a1a
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-07-31 21:37:29 +0500

    deploy

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -18,6 +18,8 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   const { email, password } = req.body;
 
   try {
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://gym-cli.onrender.com"],
+    origin: ["https://fit-track-cli.vercel.app"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
