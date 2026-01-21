521daa43cc3f2c9bf2710c70832fcf2669ac98a9
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2026-01-21 13:48:00 +0500

    test deploy

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -48,8 +48,6 @@ const createToken = (_id) => {
 
 
 const signup = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
   const { email, password } = req.body;
 
@@ -92,8 +90,6 @@ const signup = async (req, res) => {
 
 
 const login = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
   const { email, password } = req.body;
 
