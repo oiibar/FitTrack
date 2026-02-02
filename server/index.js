54de8f17fc60545564592b6288776506305016d3
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2026-02-02 18:10:08 +0500

    session auth

--- server/index.js
+++ server/index.js
@@ -50,8 +50,10 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // 1 day
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Lax'
+        // secure: process.env.NODE_ENV === 'production',
+        // sameSite: 'Lax'
+        secure: true,
+        sameSite: 'none'
     }
 }));
 
